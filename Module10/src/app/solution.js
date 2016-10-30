bd48d6a1c082eb6a197218a85e3ea731a5dce4c2
igorxut <igorxut@gmail.com>
2016-10-30 18:20:58 +0300

    Create solution.js

---
+++ Module10/src/app/solution.js
@@ -0,0 +1,17 @@
+// BEGIN (write your solution here)
+const square = (x) => {
+  return x * x;
+}
+const sumOfSquares = (x, y) => {
+  return square(x) + square(y);
+}
+const squareSumOfSquares = (x, y) => {
+  return square(sumOfSquares(x, y));
+}
+// END
+
+export default {
+  square,
+  sumOfSquares,
+  squareSumOfSquares,
+};
