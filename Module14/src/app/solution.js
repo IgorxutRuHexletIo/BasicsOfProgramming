ca5a2dfdb63b65db87f04e4db1e2708d964be9e1
igorxut <igorxut@gmail.com>
2016-10-30 18:30:06 +0300

    Create solution.js

---
+++ Module14/src/app/solution.js
@@ -0,0 +1,10 @@
+// BEGIN (write your solution here)
+export const check = (a, b, c, d) => {
+  if (a === c && b === d) {
+    return 2;
+  } else if ((a === b && c === d) || (a > b && c > d) || (a < b && c < d)) {
+    return 1;
+  }
+  return 0;
+}
+// END
