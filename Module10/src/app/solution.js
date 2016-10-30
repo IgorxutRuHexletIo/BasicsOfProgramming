f52b881d2ff527a30ccade5e8f945c502c838254
igorxut <igorxut@gmail.com>
2016-10-30 18:36:16 +0300

    Update solution.js

--- Module10/src/app/solution.js
+++ Module10/src/app/solution.js
@@ -1,13 +1,15 @@
 // BEGIN (write your solution here)
 const square = (x) => {
   return x * x;
-}
+};
+
 const sumOfSquares = (x, y) => {
   return square(x) + square(y);
-}
+};
+
 const squareSumOfSquares = (x, y) => {
   return square(sumOfSquares(x, y));
-}
+};
 // END
 
 export default {
