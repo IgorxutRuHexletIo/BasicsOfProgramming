ba85977d800144230688bb0ff5f2c969c195f755
igorxut <igorxut@gmail.com>
2016-10-30 18:18:44 +0300

    Create test.js

---
+++ Module9/src/app/test.js
@@ -0,0 +1,6 @@
+import assert from 'assert';
+import squareOfSum from './solution';
+
+assert.equal(1, squareOfSum(1, 0));
+assert.equal(25, squareOfSum(8, -3));
+assert.equal(16, squareOfSum(2, 2));
