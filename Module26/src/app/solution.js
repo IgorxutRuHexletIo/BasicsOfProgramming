19822d7d9f8f673e4e579e2a1f63c80a593f61cc
igorxut <igorxut@gmail.com>
2016-10-30 19:02:13 +0300

    Create solution.js

---
+++ Module26/src/app/solution.js
@@ -0,0 +1,42 @@
+import * as strings from './strings';
+
+const bigLettersCount = (str) => {
+  // BEGIN (write your solution here)
+  let len = strings.length(str);
+  let bl_count = 0;
+
+  for (let i = 0; i < len; i++) {
+    if (str[i] === strings.toUpperCase(str[i])) {
+      bl_count++;
+    }
+  }
+
+  return bl_count;
+  // END
+};
+
+
+const compare = (first, second) => {
+  const firstCount = bigLettersCount(first);
+  const secondCount = bigLettersCount(second);
+
+  // BEGIN (write your solution here)
+  if (firstCount > secondCount) {
+    return 1;
+  } else if (firstCount < secondCount) {
+    return -1;
+  }
+
+  return 0;
+  }
+  // END
+};
+
+export const greaterThan = (first, second) => {
+  return compare(first, second) === 1;
+};
+
+export const lessThan = (first, second) => {
+  return compare(first, second) === -1;
+};
+// END
