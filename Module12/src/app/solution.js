18eddc3c3c515d84b570d1234b9dfb502964c11a
igorxut <igorxut@gmail.com>
2016-10-30 18:36:53 +0300

    Update solution.js

--- Module12/src/app/solution.js
+++ Module12/src/app/solution.js
@@ -1,9 +1,9 @@
 // BEGIN (write your solution here)
 const degToRad = (deg) => {
   return deg * Math.PI / 180;
-}
+};
 
 export default (a, b, c) => {
   return a * a / 2 * Math.sin(degToRad(b)) * Math.sin(degToRad(c)) / Math.sin(degToRad(180 - b - c));
-}
+};
 // END
